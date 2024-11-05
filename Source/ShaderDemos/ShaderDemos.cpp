1ffe766a2759bcd7a91c58635f44e5a10a49ec53
Josh Kabo <joshkong1@gmail.com>
2024-11-04 23:35:30 -0800

    init

---
+++ Source/ShaderDemos/ShaderDemos.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ShaderDemos.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ShaderDemos, "ShaderDemos" );
---
+++ Source/ShaderDemos/ShaderDemosGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ShaderDemosGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SHADERDEMOS_API AShaderDemosGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
